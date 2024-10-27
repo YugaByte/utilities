495ecb8361068d40fc5355439887f2082a9a51e2
Franck Pachot <mail@pachot.net>
2024-10-27 22:43:09 +0000

    fix quotes

--- common_scripts/create_universe.sh
+++ common_scripts/create_universe.sh
@@ -47,7 +47,7 @@ case "${CLOUD_NAME}" in
 
   'AWS')
     TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
-    NODEIP="$(curl -H 'X-aws-ec2-metadata-token: $TOKEN' http://169.254.169.254/latest/meta-data/local-ipv4)"
+    NODEIP="$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4)"
     ;;
 
   'Azure')
